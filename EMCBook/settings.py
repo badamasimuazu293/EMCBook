91da7c82d4298484d950a728791160a39eb9785d
Muaz <badamasimuazu293@gmail.com>
2026-05-13 06:15:53 -0700

    debug enabled

--- EMCBook/settings.py
+++ EMCBook/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'EMCBook.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3' )
+         default='sqlite:///db.sqlite3' )
 }
 
 
